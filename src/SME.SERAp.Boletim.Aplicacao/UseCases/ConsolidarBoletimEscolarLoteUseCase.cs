27c9c71976d3619029800416a2a0e5b60997d992
Mario Junior <mariosj9@gmail.com>
2025-05-23 14:49:07 -0300

    Correção de duplicidade de consolidação

--- src/SME.SERAp.Boletim.Aplicacao/UseCases/ConsolidarBoletimEscolarLoteUseCase.cs
+++ src/SME.SERAp.Boletim.Aplicacao/UseCases/ConsolidarBoletimEscolarLoteUseCase.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
+using SME.SERAp.Boletim.Aplicacao.Commands.LoteProva.AlterarStatusConsolidacao;
 using SME.SERAp.Boletim.Aplicacao.Commands.PublicaFilaRabbit;
 using SME.SERAp.Boletim.Aplicacao.Interfaces;
+using SME.SERAp.Boletim.Dominio.Enums;
 using SME.SERAp.Boletim.Infra.Fila;
 
 namespace SME.SERAp.Boletim.Aplicacao.UseCases
@@ -21,11 +23,12 @@ namespace SME.SERAp.Boletim.Aplicacao.UseCases
                 return;
 
             _lotesConsolidar.Add(loteId);
+            await mediator.Send(new AlterarLoteProvaStatusConsolidacaoCommand(loteId, LoteStatusConsolidacao.Pendente));
 
             _ = Task.Run(async () =>
             {
                 await Task.Delay(TimeSpan.FromMinutes(2));
-                await mediator.Send(new PublicaFilaRabbitCommand(RotasRabbit.BuscarProvasBoletimLote, loteId));
+                await mediator.Send(new PublicaFilaRabbitCommand(RotasRabbit.BuscarProvasBoletimLote));
                 _lotesConsolidar.Remove(loteId);
             });
         }
--- src/SME.SERAp.Boletim.Dados/Interfaces/IRepositorioLoteProva.cs
+++ src/SME.SERAp.Boletim.Dados/Interfaces/IRepositorioLoteProva.cs
@@ -1,10 +1,12 @@
 ﻿using SME.SERAp.Boletim.Dominio.Entities;
+using SME.SERAp.Boletim.Dominio.Enums;
 
 namespace SME.SERAp.Boletim.Dados.Interfaces
 {
     public interface IRepositorioLoteProva : IRepositorioBase<LoteProva>
     {
         Task<int> DesativarTodosLotesProvaAtivos();
+        Task<int> AlterarStatusConsolidacao(long idLotProva, LoteStatusConsolidacao loteStatusConsolidacao);
         Task<IEnumerable<LoteProva>> ObterLotesProvaPorData(DateTime inicio, DateTime fim, bool formataTai);
     }
 }
