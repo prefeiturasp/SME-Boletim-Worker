86ec30a1f5bf973a10be7c3bf629fcd5df0b530b
Mario Junior <mariosj9@gmail.com>
2025-08-22 15:08:09 -0300

    AB#133022 EnvironmentVariables e Exceptions

---
+++ teste/SME.SERAp.Boletim.Infra.Testes/EnvironmentVariables/RabbitLogOptionsTeste.cs
@@ -0,0 +1,32 @@
+﻿using SME.SERAp.Boletim.Infra.EnvironmentVariables;
+
+namespace SME.SERAp.Boletim.Infra.Testes.EnvironmentVariables
+{
+    public class RabbitLogOptionsTeste
+    {
+        [Fact]
+        public void Deve_Retornar_Valor_Secao_Esperado()
+        {
+            var secao = RabbitLogOptions.Secao;
+
+            Assert.Equal("RabbitLog", secao);
+        }
+
+        [Fact]
+        public void Deve_Atribuir_Propriedades_Corretamente()
+        {
+            var options = new RabbitLogOptions
+            {
+                HostName = "localhost",
+                UserName = "user",
+                Password = "bitnami",
+                VirtualHost = "/"
+            };
+
+            Assert.Equal("localhost", options.HostName);
+            Assert.Equal("user", options.UserName);
+            Assert.Equal("bitnami", options.Password);
+            Assert.Equal("/", options.VirtualHost);
+        }
+    }
+}
---
+++ teste/SME.SERAp.Boletim.Infra.Testes/EnvironmentVariables/RabbitOptionsTeste.cs
@@ -0,0 +1,36 @@
+﻿using SME.SERAp.Boletim.Infra.EnvironmentVariables;
+
+namespace SME.SERAp.Boletim.Infra.Testes.EnvironmentVariables
+{
+    public class RabbitOptionsTeste
+    {
+        [Fact]
+        public void Deve_Retornar_Valor_Secao_Esperado()
+        {
+            var secao = RabbitOptions.Secao;
+
+            Assert.Equal("Rabbit", secao);
+        }
+
+        [Fact]
+        public void Deve_Atribuir_Propriedades_Corretamente()
+        {
+            var options = new RabbitOptions
+            {
+                HostName = "localhost",
+                UserName = "user",
+                Password = "bitnami",
+                VirtualHost = "/",
+                LimiteDeMensagensPorExecucao = 50,
+                ForcarRecriarFilas = true
+            };
+
+            Assert.Equal("localhost", options.HostName);
+            Assert.Equal("user", options.UserName);
+            Assert.Equal("bitnami", options.Password);
+            Assert.Equal("/", options.VirtualHost);
+            Assert.Equal((ushort)50, options.LimiteDeMensagensPorExecucao);
+            Assert.True(options.ForcarRecriarFilas);
+        }
+    }
+}
