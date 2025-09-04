a2da5e777727921a8e7ef1c25edd46e3be614aa1
Mario Junior <mariosj9@gmail.com>
2025-09-04 11:39:13 -0300

    AB#133535

---
+++ src/SME.SERAp.Boletim.Aplicacao/UseCases/BuscarAlunoProvaSpProficienciaUseCase.cs
@@ -0,0 +1,75 @@
+﻿using MediatR;
+using SME.SERAp.Boletim.Aplicacao.Commands.PublicaFilaRabbit;
+using SME.SERAp.Boletim.Aplicacao.Interfaces;
+using SME.SERAp.Boletim.Aplicacao.Queries;
+using SME.SERAp.Boletim.Dominio.Entities;
+using SME.SERAp.Boletim.Infra.Fila;
+using SME.SERAp.Boletim.Infra.Interfaces;
+
+namespace SME.SERAp.Boletim.Aplicacao.UseCases
+{
+    public class BuscarAlunoProvaSpProficienciaUseCase : IBuscarAlunoProvaSpProficienciaUseCase
+    {
+        private readonly IMediator mediator;
+        private readonly IServicoLog servicoLog;
+
+        public BuscarAlunoProvaSpProficienciaUseCase(IMediator mediator, IServicoLog servicoLog)
+        {
+            this.mediator = mediator;
+            this.servicoLog = servicoLog;
+        }
+
+        public async Task<bool> Executar(MensagemRabbit mensagemRabbit)
+        {
+            try
+            {
+                var boletimProvaAluno = mensagemRabbit.ObterObjetoMensagem<BoletimProvaAluno>();
+                if (boletimProvaAluno is null)
+                    throw new Exception("Mensagem inválida.");
+
+                var areaDoConhecimentoId = ObterAreaDoConhecimento(boletimProvaAluno.DisciplinaId);
+                var alunoMatricula = boletimProvaAluno.AlunoRa.ToString();
+                var edicaoProvaSp = await ObterEdicaoProvaSp(boletimProvaAluno.ProvaId);
+
+                var alunosProvaSpProficiencia = await mediator
+                    .Send(new ObterResultadoAlunoProvaSpQuery(edicaoProvaSp, areaDoConhecimentoId, alunoMatricula));
+
+                if (alunosProvaSpProficiencia is null)
+                    return true;
+
+                await mediator.Send(new PublicaFilaRabbitCommand(RotasRabbit.TratarAlunoProvaSpProficiencia, alunosProvaSpProficiencia));
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                servicoLog.Registrar(ex);
+                return false;
+            }
+        }
+
+        private async Task<int> ObterEdicaoProvaSp(long provaId)
+        {
+            var anoProva = await mediator.Send(new ObterAnoProvaQuery(provaId));
+            if(anoProva is null || anoProva == 0)
+                throw new Exception($"Não foi possível identificar o ano da prova. ProvaId: {provaId}");
+
+            var edicao = anoProva.Value - 1;
+            return edicao;
+        }
+
+        private int ObterAreaDoConhecimento(long disciplinaId)
+        {
+            return disciplinaId switch
+            {
+                //Matemática
+                4 => 3,
+                //Língua Portuguesa
+                5 => 2,
+                //Ciências
+                2 or 6 or 7 => 1,
+                _ => 0,
+            };
+        }
+    }
+}
